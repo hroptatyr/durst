92b4c21e7b14053de094a8167066e86e361d8279
Sebastian Freundt <freundt@ga-group.nl>
2011-10-04 10:51:22 +0000

    fix, do not remove source files
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/dt-test.sh
+++ test/dt-test.sh
@@ -36,9 +36,29 @@ pwd=$(pwd)
 ## source the check
 . "${1}" || fail=1
 
+rm_if_not_src()
+{
+	file="${1}"
+	srcd="${2:-${srcdir}}"
+	dirf=$(dirname "${file}")
+
+	if test "${dirf}" = "." -a "$(pwd)" = "${srcd}" -a -r "${file}"; then
+		## treat as precious source file
+		:
+	elif test "${dirf}" = "${srcd}"; then
+		## treat as precious source file
+		:
+	else
+		rm -vf -- "${file}"
+	fi
+}
+
 myexit()
 {
-	rm -f -- "${stdin}" "${stdout}" "${stderr}" "${tool_stdout}" "${tool_stderr}"
+	rm_if_not_src "${stdin}" "${srcdir}"
+	rm_if_not_src "${stdout}" "${srcdir}"
+	rm_if_not_src "${stderr}" "${srcdir}"
+	rm -f -- "${tool_stdout}" "${tool_stderr}"
 	cd "${pwd}"
 	exit ${1:-1}
 }
@@ -57,6 +77,7 @@ if test -x "${builddir}/${TOOL}"; then
 	TOOL=$(readlink -e "${builddir}/${TOOL}")
 fi
 
+srcdir=$(readlink -e "${srcdir}")
 cd "${srcdir}"
 eval "${TOOL}" "${CMDLINE}" \
 	< "${stdin:-/dev/null}" \
