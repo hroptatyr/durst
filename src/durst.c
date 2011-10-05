95fd0ed95f78076bc3dcea7059c99661dd8551e5
Sebastian Freundt <freundt@ga-group.nl>
2011-10-05 09:29:06 +0000

    fix, initialise soft position for futures
    This fixes a bug reported by Ruediger Meier where evaluating a slot with
    uninitialised data caused the data_complete_p() predicate to fail.
    
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/durst.c
+++ src/durst.c
@@ -774,7 +774,7 @@ __parse_fut(urs_fut_pos_t fp, const char *line)
 
 	/* frob soft_pos */
 	line = __skip_behind_tab(p);
-	fp->pos.hard = read_tab_double(p = line);
+	fp->pos.soft = read_tab_double(p = line);
 
 	/* frob hard_pos */
 	line = __skip_behind_tab(p);
